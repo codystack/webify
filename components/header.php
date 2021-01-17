637ac6f920a5737a529df01784454d93467af15d
codystack <ebuzzadvert@gmail.com>
2021-01-17 08:22:26 +0100

    V1

---
+++ components/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+    
+<head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Webify&trade; :: Brand Identity, User Experience Design, App Development &amp; Web Development</title>
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:url" content="https://webify.com.ng/"/>
+        <meta property="og:site_name" content="Webify" />
+	    <meta property="og:type" content="website" />
+        <meta property="og:title" content="TG&trade; :: Web + App Developer, UI/UX Designer, Product Designer & Graphic Designer." />
+        <meta name="og:description" content="Webify is a digital/branding agency that collaborates with brands to increase their value and impact through branding, user interface and experience design, app development and web development.">
+        <meta name="keywords" content="Creative Agency,Lagos,Nigeria,Port Harcourt,Website Development,Application Development,Software Development,Build Website,Wordpress,Software,Developer,Development,Programmer,Programming,Coder,Coding,Blogger,Blogging,Digital,Creator,Creation,Graphic Designer,Product Designer,Product,Graphics,Branding,Lekki,Abuja,Bootstrap,Angular,Vue,Frontend,Front-end,UX,UI,UI/UX">
+        <meta property="og:image" content="https://i.imgur.com/uIvHnMQ.jpg">
+        <link rel="icon" type="image/png" href="https://i.imgur.com/ZqEiVRm.png">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <!-- Google Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&amp;display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="assets/vendor/swiper/dist/css/swiper.min.css" />
+        <link rel="stylesheet" href="assets/vendor/fancybox/dist/jquery.fancybox.min.css" />
+        <link rel="stylesheet" href="assets/css/themebau.min.css">
+        <link rel="stylesheet" href="assets/css/custom.css">
+        <!-- END: Styles -->
+        <!-- jQuery -->
+        <script src="assets/vendor/jquery/dist/jquery.min.js"></script>
+    </head>
+    <body>
