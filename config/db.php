4da9b76027ddb2d325589e3519da52a5f15d4886
codystack <ebuzzadvert@gmail.com>
2021-01-23 13:57:16 +0100

    coming soon

--- config/db.php
+++ config/db.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli('localhost', 'root', 'Webify2020!!', 'webify');
+$conn = new mysqli('localhost', 'webiirem_webify', 'Webify2020!!', 'webiirem_webify');
 error_reporting(E_ALL ^ E_NOTICE); // turn ON showing errors
 // Check connection
 if (mysqli_connect_errno()) {
